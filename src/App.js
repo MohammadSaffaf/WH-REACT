4b53edf6b7798fcf63b9100c400dbaec87e3f1bf
MohammadSaffaf <mhmdsaffha@gmail.com>
2021-01-27 14:21:53 +0100

    StateFaq

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Counter from './components/Counter';
+import StateFaq from './components/StateFaq';
 
 function App() {
   return (
     <article>
       <Counter/>
+      <StateFaq/>
     </article>
   );
 }
---
+++ src/components/StateFaq.js
@@ -0,0 +1,19 @@
+import { useState} from "react";
+ const StateFaq = () => {
+    const [hidden, setHidden] = useState(false);
+     return ( 
+         <article className="counter-article">
+             <section className="state-section">
+             <div className="state-div"> 
+                 <h1> Why is React greate</h1>
+                 <button onClick={() => setHidden(!hidden)}>+</button>
+                 </div>
+             <div hidden={hidden} className="hidden">
+                 <h2>Fast Learning Curve</h2>
+             </div>
+             </section>
+         </article>
+      );
+ }
+  
+ export default StateFaq;
